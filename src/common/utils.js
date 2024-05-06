6da54eacbdca3c3e72fef1f03cfea92b98ca62b3
da_b1rmuda <lamurka1337@yandex.ru>
2024-05-06 19:04:55 +0400

    v1.0.5

--- src/common/utils.js
+++ src/common/utils.js
@@ -4,7 +4,7 @@ import { useEffect, useLayoutEffect, useState } from "react";
 import packageJson from "../../package.json";
 export { localforage };
 
-export const HEADER_TITLE = "HEADER_TITLE goes here hi Lesha";
+export const HEADER_TITLE = "HEADER_TITLE goes here hi Denis";
 export const FOOTER = "FOOTER goes here";
 export const VERSION = packageJson.version;
 
